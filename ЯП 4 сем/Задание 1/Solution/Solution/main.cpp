920859d596c4c3234a82fad40db43112cca703d5
I461-TKI <lukankinv20@gmail.com>
2025-04-01 22:25:15 +0300

    Удалил ненужные проекты и сделал реализацию (filename.txt - это файл выхода)

--- ЯП 4 сем/Задание 1/Solution/Solution/main.cpp
+++ ЯП 4 сем/Задание 1/Solution/Solution/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
 
 /**
 * @brief Точка входа в программу
 */
-int main()
-{
-	return 0;
+int main() {
+    std::string filename;
+    std::cin >> filename;
+
+    std::ofstream outFile(filename);
+    if (!outFile) {
+        std::cerr << "Ошибка при открытии файла!" << std::endl;
+        return 1;
+    }
+
+    std::ostream_iterator<char> outIter(outFile, " ");
+    std::copy(std::istream_iterator<char>(std::cin),
+        std::istream_iterator<char>(),
+        outIter);
+
+    outFile.close();
+    return 0;
 }
