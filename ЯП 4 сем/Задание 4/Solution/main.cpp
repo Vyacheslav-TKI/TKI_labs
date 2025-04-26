156f1f75af93060b2e38dd9b12269e619361b9fb
I461-TKI <lukankinv20@gmail.com>
2025-04-26 23:09:19 +0300

    Задание 4

--- ЯП 4 сем/Задание 4/Solution/main.cpp
+++ ЯП 4 сем/Задание 4/Solution/main.cpp
@@ -1,4 +1,60 @@
-int main()
-{
-	return 0;
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>   
+#include <functional> 
+
+
+struct point {
+    double x, y;
+
+    friend std::istream& operator>>(std::istream& is, point& p) {
+        return is >> p.x >> p.y;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const point& p) {
+        return os << "(" << p.x << ", " << p.y << ")";
+    }
+
+    bool operator<(const point& other) const {
+        return (x < other.x) || (x == other.x && y < other.y);
+    }
+};
+
+int main() {
+    const char* filename = "name";     
+    std::ifstream fin(filename);
+    if (!fin) {
+        std::cerr << "Не удалось открыть файл " << filename << "\n";
+        return 1;
+    }
+
+
+    std::vector<point> V;
+    point p;
+    while (fin >> p) {
+        V.push_back(p);
+    }
+
+
+    point origin{ 0.0, 0.0 };
+
+
+    using namespace std::placeholders;
+    auto is_less_than_origin =
+        std::bind(std::less<point>(), _1, origin);
+
+    std::stable_partition(
+        V.begin(), V.end(),
+        is_less_than_origin
+    );
+
+
+    std::for_each(
+        V.begin(), V.end(),
+        [](const point& q) {
+            std::cout << q << "\n";
+        }
+    );
+
+    return 0;
 }
